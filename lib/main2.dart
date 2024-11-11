bd2cb9acb64bebf643cec80c2ce7e1d2e5b61e43
wattanapong.su <wattanapong.su@outlook.com>
2024-11-12 00:50:58 +0700

    add smallest and column, row

---
+++ lib/main2.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'calculate_utils.dart';
+
+void main() => runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Scaffold Row')),
+        ),
+        body: const Center(
+          // child: Column(
+          //     children: [
+          //       Text('Top'),
+          //       Text('Middle'),
+          //       Text('Bottom'),
+          //     ]
+          // ),
+          child: Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(width: 20),
+                Text('Left  '),
+                SizedBox(width: 20),
+                Text('Center  '),
+                SizedBox(width: 20),
+                Text('Right '),
+              ]),
+        ))));
